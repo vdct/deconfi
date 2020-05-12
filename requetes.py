d1accd18aae3d23af47c4668134c642d6c5283d4
Vincent de Château-Thierry <vdct@laposte.net>
2020-05-12 22:09:59 +0000

    Simplification des contours & iles

--- requetes.py
+++ requetes.py
@@ -11,20 +11,37 @@ def get_zone(lat,lon,epsg):
                         AS
                         (SELECT (ST_Union(geometrie)) geometrie
                         FROM
-                        (SELECT geometrie FROM depts_geo WHERE ST_Contains(geometrie,ST_SetSRID(ST_MakePoint({lon},{lat}),4326))
+                        (SELECT geometrie FROM dept_geo WHERE ST_Contains(geometrie,ST_SetSRID(ST_MakePoint({lon},{lat}),4326))
                         UNION ALL
                         SELECT ST_Transform(ST_Buffer(ST_Transform(ST_SetSRID(ST_MakePoint({lon},{lat}),4326),{epsg}),100000),4326))a),
                         zone_stricte
                         AS
                         (SELECT ST_Intersection(zf.geometrie,fr.geometrie) geometrie
                         FROM zone_full AS zf
-                        JOIN contours_france_geo AS fr
+                        JOIN contour_france_geo AS fr
                         ON zf.geometrie && fr.geometrie)
                         SELECT ST_AsGeoJSON(ST_Union(geometrie),5)
                         FROM zone_stricte
                      """)
 
         return((cur.fetchone()[0]))
+
+def get_common_part(geojson1,geojson2):
+    with db.bano_cache.cursor() as cur:
+        cur.execute(f"""
+                        WITH
+                        z1
+                        AS
+                        (SELECT ST_SetSRID(ST_MakeValid(ST_GeomFromGeoJSON('{geojson1}')),4326) AS geometrie),
+                        z2
+                        AS
+                        (SELECT ST_SetSRID(ST_MakeValid(ST_GeomFromGeoJSON('{geojson2}')),4326) AS geometrie)
+                        SELECT ST_AsGeoJSON(ST_Intersection(z1.geometrie,z2.geometrie),5)
+                        FROM z1,z2
+                     """)
+
+        return((cur.fetchone()[0]))
+
 
 def get_commune(lat,lon):
     with db.bano_cache.cursor() as cur:
@@ -67,3 +84,18 @@ def get_longest_line(lat,lon,epsg,geojson):
                      """)
         return((cur.fetchone()))
 
+def get_epsgcode_by_dept(dept):
+    epsg = 2154
+    if len(dept) !=2:
+        epsg = 3857
+    if dept == '971':
+        epsg = 4559
+    if dept == '972':
+        epsg = 4559
+    if dept == '973':
+        epsg = 2972
+    if dept == '974':
+        epsg = 2975
+    if dept == '976':
+        epsg = 4471
+    return epsg
--- sql/contours_france.sql
+++ sql/contours_france.sql
@@ -1,6 +1,26 @@
-DROP TABLE IF EXISTS contours_france_geo CASCADE;
-CREATE TABLE contours_france_geo
+CREATE TEMP TABLE 
+poly_dump
 AS
-SELECT ST_SetSRID(ST_Subdivide(ST_SimplifyPreserveTopology(ST_Union(geometrie),0.001),60000),4326) geometrie
+SELECT (ST_Dump(geometrie)).geom geometrie,code_insee
 FROM depts_geo;
-CREATE INDEX gidx_contours_france_geo ON contours_france_geo USING GIST(geometrie);
+
+DROP TABLE IF EXISTS dept_geo CASCADE;
+CREATE TABLE dept_geo
+AS
+SELECT ST_Union(geometrie) AS geometrie,
+       code_insee
+FROM poly_dump
+WHERE ST_Area(ST_Transform(geometrie,3857)) > 8000 and ST_NPoints(geometrie) > 5
+GROUP BY code_insee;
+
+CREATE INDEX gidx_dept_geo
+ON dept_geo USING GIST(geometrie);
+
+DROP TABLE IF EXISTS contour_france_geo CASCADE;
+CREATE TABLE contour_france_geo
+AS
+SELECT ST_SetSRID(ST_Subdivide(ST_SimplifyPreserveTopology(ST_Union(geometrie),0.001),6000),4326) geometrie
+FROM dept_geo;
+
+CREATE INDEX gidx_contour_france_geo
+ON contour_france_geo USING GIST(geometrie);
